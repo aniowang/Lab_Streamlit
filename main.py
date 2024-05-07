1ac9b70a0b29f6f35d9c329344179dd94c543e48
aniowang <140711934+aniowang@users.noreply.github.com>
2024-05-07 13:17:32 +0800

    初始化佈署
    第一次上傳試驗用程式

---
+++ main.py
@@ -0,0 +1,53 @@
+# Contents of ~/my_app/streamlit_app.py
+import streamlit as st
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+import pandas as pd
+
+with open('./config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['pre-authorized']
+)
+
+def main_page():
+    st.markdown("# Main page 🎈")
+    
+    #添加側邊攔
+    st.sidebar.write('測試版本：V0.0.1') 
+    st.sidebar.write('測試時間：',pd.Timestamp.now()) 
+    
+    
+    
+    
+    
+    
+    
+
+if __name__=="__main__": 
+    if st.session_state["authentication_status"] is None:
+        """
+        此為測試登入功能以及各頁面檢視權限管控之實驗頁面
+    
+        請先登入後方可分頁檢視
+        """
+    #修改登入介面文字
+    authenticator.login( 
+        location='main',
+        fields={'Form name':'登入', 'Username':'使用者名稱', 'Password':'密碼', 'Login':'確認登入'})  
+    
+    if st.session_state["authentication_status"]:
+        authenticator.logout(location='sidebar',button_name='確認登出')
+        st.sidebar.write(f'Welcome *{st.session_state["name"]}*')
+        #登入成功後才執行main()
+        main_page()         
+    elif st.session_state["authentication_status"] is False:
+        st.error('您輸入的帳號/密碼 錯誤')
+    elif st.session_state["authentication_status"] is None:
+        st.warning('請輸入您的帳號及密碼')
---
+++ pages/分頁1.py
@@ -0,0 +1,47 @@
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+import streamlit as st
+import pandas as pd
+
+with open('./config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['pre-authorized']
+)
+
+def page1():
+    
+
+    
+    st.write('測試時間：',pd.Timestamp.now())
+    
+    #添加側邊攔
+    st.sidebar.write('測試版本：V0.0.1') 
+    st.sidebar.write('測試時間：',pd.Timestamp.now()) 
+
+if __name__=="__main__":
+    if st.session_state["authentication_status"] is None:
+        """
+        此為測試登入功能以及各頁面檢視權限管控之實驗頁面
+    
+        請先登入後方可分頁檢視
+        """
+    #修改登入介面文字
+    authenticator.login( 
+        location='main',
+        fields={'Form name':'登入', 'Username':'使用者名稱', 'Password':'密碼', 'Login':'確認登入'})    
+    if st.session_state["authentication_status"]:
+        authenticator.logout(location='sidebar',button_name='確認登出')
+        st.sidebar.write(f'Welcome *{st.session_state["name"]}*')
+        #登入成功後才執行page1()
+        page1()         
+    elif st.session_state["authentication_status"] is False:
+        st.error('您輸入的帳號/密碼 錯誤')
+    elif st.session_state["authentication_status"] is None:
+        st.warning('請輸入您的帳號及密碼')

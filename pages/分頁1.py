ffea4d0468519d8561ac692124532d46d4a6d827
aniowang <140711934+aniowang@users.noreply.github.com>
2024-05-10 14:50:11 +0800

    Update 分頁1.py

--- pages/分頁1.py
+++ pages/分頁1.py
@@ -31,6 +31,7 @@ def page1():
     
         # Every form must have a submit button
         submitted = st.form_submit_button('Submit')
+        st.toast('確認送出囉!')
     
     #添加側邊攔
     st.sidebar.write('測試版本：V0.0.1') 

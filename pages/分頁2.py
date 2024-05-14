1d3f7e3d5d478e25eadd2a82e23418825de72734
aniowang <140711934+aniowang@users.noreply.github.com>
2024-05-14 15:05:56 +0800

    Update 分頁2.py

--- pages/分頁2.py
+++ pages/分頁2.py
@@ -29,20 +29,16 @@ def page2():
     #添加側邊攔
     st.sidebar.write('測試版本：V0.0.1') 
     st.sidebar.write('測試時間：',pd.Timestamp.now(tz='Asia/Shanghai')) 
-    n = None    
+    n = None 
+
+    df = load_data("https://github.com/plotly/datasets/raw/master/uber-rides-data1.csv")  
+    n=np.random.randint(1,20)
+    st.write('隨機顯示行數：',n)
+    st.dataframe(df.head(n))
     
     #點擊按鈕後刷新頁面
     if st.button("Rerun"):
         st.experimental_rerun()
-    
-    if st.button("下載資料集"):
-        df = load_data("https://github.com/plotly/datasets/raw/master/uber-rides-data1.csv")
-
-    if 'df' in globals():
-        n=np.random.randint(1,20)
-        st.write('隨機顯示行數：',n)
-        st.dataframe(df.head(n))
-
 
     #st.toast('頁面已更新')
 

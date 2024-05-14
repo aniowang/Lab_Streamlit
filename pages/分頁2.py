6387fda7148a6477c905b4e41978a03ef279e53c
aniowang <140711934+aniowang@users.noreply.github.com>
2024-05-14 15:32:36 +0800

    Update 分頁2.py

--- pages/分頁2.py
+++ pages/分頁2.py
@@ -33,7 +33,7 @@ def page2():
     #判斷目前session是否有存在
     if 'df' not in st.session_state:
         st.session_state.df = load_data("https://github.com/plotly/datasets/raw/master/uber-rides-data1.csv")  
-        st.info('資料已下載')
+        st.toast('資料已下載')
         
     #建立更新/預覽按鈕
     if st.button("更新/預覽已下載數據"):    

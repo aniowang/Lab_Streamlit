f1d50289001f30826663fb7a829d8e7cd0fbbb2b
aniowang <140711934+aniowang@users.noreply.github.com>
2024-05-14 15:19:33 +0800

    Update 分頁2.py

--- pages/分頁2.py
+++ pages/分頁2.py
@@ -29,14 +29,17 @@ def page2():
     #添加側邊攔
     st.sidebar.write('測試版本：V0.0.1') 
     st.sidebar.write('測試時間：',pd.Timestamp.now(tz='Asia/Shanghai')) 
-    n = None 
-
-    df = load_data("https://github.com/plotly/datasets/raw/master/uber-rides-data1.csv")      
+    # n = None 
+    if 'df' not in st.session_state:
+        df = load_data("https://github.com/plotly/datasets/raw/master/uber-rides-data1.csv")      
+        
+    n=np.random.randint(1,20)
+    st.dataframe(st.session_state.df.head(n))
     
-    if st.button("預覽已下載數據"):    
+    if st.button("更新預覽已下載數據"):    
         n=np.random.randint(1,20)
         st.write('隨機顯示行數：',n)
-        st.dataframe(df.head(n))
+        st.dataframe(st.session_state.df.head(n))
 
     
     

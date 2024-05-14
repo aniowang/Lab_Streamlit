6734552f2d0d794d7c9a763029a765c3d22dc2d6
aniowang <140711934+aniowang@users.noreply.github.com>
2024-05-14 15:12:44 +0800

    Update 分頁2.py

--- pages/分頁2.py
+++ pages/分頁2.py
@@ -31,13 +31,14 @@ def page2():
     st.sidebar.write('測試時間：',pd.Timestamp.now(tz='Asia/Shanghai')) 
     n = None 
 
-    df = load_data("https://github.com/plotly/datasets/raw/master/uber-rides-data1.csv")  
+    df = load_data("https://github.com/plotly/datasets/raw/master/uber-rides-data1.csv")      
     
-    if 'df' in globals():
-        st.write("預覽已下載數據")    
+    if st.button("預覽已下載數據"):    
         n=np.random.randint(1,20)
         st.write('隨機顯示行數：',n)
         st.dataframe(df.head(n))
+
+    
     
     #點擊按鈕後刷新頁面
     if st.button("Rerun"):

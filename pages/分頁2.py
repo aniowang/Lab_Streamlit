2863a31e52cc034fc8972abc60bb34b2e3c08444
aniowang <140711934+aniowang@users.noreply.github.com>
2024-05-14 16:21:20 +0800

    Update 分頁2.py

--- pages/分頁2.py
+++ pages/分頁2.py
@@ -36,11 +36,11 @@ def page2():
             st.session_state.df = load_data("https://github.com/plotly/datasets/raw/master/uber-rides-data1.csv")  
             st.toast('資料已下載')
         
-    #建立更新/預覽按鈕
-    if st.button("更新/預覽已下載數據"):    
-        n=np.random.randint(1,20)
-        st.write('隨機顯示行數：',n)
-        st.dataframe(st.session_state.df.head(n))    
+        #建立更新/預覽按鈕
+        if st.button("更新/預覽已下載數據"):    
+            n=np.random.randint(1,20)
+            st.write('隨機顯示行數：',n)
+            st.dataframe(st.session_state.df.head(n))    
     
     #點擊按鈕後刷新頁面
     if st.button("Rerun"):

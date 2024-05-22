a2026d778c165a0d2571671b491080928e9ab4e1
aniowang <140711934+aniowang@users.noreply.github.com>
2024-05-22 22:32:00 +0800

    Update 分頁2.py

--- pages/分頁2.py
+++ pages/分頁2.py
@@ -4,7 +4,7 @@ from yaml.loader import SafeLoader
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+import plotly.express as px
 
 with open('./config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)

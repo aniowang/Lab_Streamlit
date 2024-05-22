f781a2c17bf7992bc554ea4169f5aa95b607762e
aniowang <140711934+aniowang@users.noreply.github.com>
2024-05-22 22:36:55 +0800

    Update 分頁3.py

--- pages/分頁3.py
+++ pages/分頁3.py
@@ -5,6 +5,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import sqlite3
+import plotly.express as px
 
 """
     測試讀取Chinook (sqlite)  
@@ -134,6 +135,11 @@ def page3():
         st.write("專輯相關銷售紀錄：",Txn)
     else:
         st.warning("無專輯銷售紀錄")
+
+    col1,col2 = st.columns(2)
+
+    with col1:
+        fig=px.bar(Txn,x="InvoiceLineId",y="Bytes")
 
     
 if __name__=="__main__":
